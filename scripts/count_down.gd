255b848fcb4943406c398054f1ffb2671c8bd35d
ygcs796 <yurigabryel3@gmail.com>
2026-07-23 17:56:52 -0300

    feat: count-down colorido
    Regra:
    Menor que 10: vermelho
    Menor que 20: amarelo
    Menor que 30: verde

--- scripts/count_down.gd
+++ scripts/count_down.gd
@@ -7,8 +7,13 @@ signal countdown_finished
 func _ready() -> void:
 	for segundo in range(30, -1, -1):
 		if segundo < 10:
+			count_down.add_theme_color_override("font_color", Color.RED)
 			count_down.text = "00:0" + str(segundo)
 		else:
+			if segundo < 20:
+				count_down.add_theme_color_override("font_color", Color.YELLOW)
+			else:
+				count_down.add_theme_color_override("font_color", Color.GREEN)
 			count_down.text = "00:" + str(segundo)
 		await get_tree().create_timer(1.0).timeout
 	countdown_finished.emit()
