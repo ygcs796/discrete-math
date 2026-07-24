f855284fe43e1157328284af7ad31977bb7b421d
ygcs796 <yurigabryel3@gmail.com>
2026-07-23 21:22:51 -0300

    Música
    Adicionei música no jogo. Por enquanto, só tem ela e ela vai ficar em loop.
    
    Créditos:
    "To build events" by [N]onsense (https://soundcloud.com/n-onsense/to-build-events)
    
    Também vou colocar os créditos no itch.io

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -3,8 +3,8 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	MusicManager.play(preload("res://assets/musics/to_build_events.ogg"))
 	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
